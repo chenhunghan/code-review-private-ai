0cec07b9ff2616b1201383ab7cc713402d1bfd8b
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2023-07-24 23:01:25 +0300

    Should use model file bin name, remove getMaxPromptLength
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- src/review/prompt/getMaxPromptLength.ts
+++
@@ -1,9 +0,0 @@
-
-export const getMaxPromptLength = (modelName: string): number => {
-  if (!process.env.CONTEXT_LENGTH) {
-    // Default context length for the basic llama-2-7b-chat model
-    // See https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML
-    return 4000;
-  }
-  return parseInt(process.env.CONTEXT_LENGTH);
-};
